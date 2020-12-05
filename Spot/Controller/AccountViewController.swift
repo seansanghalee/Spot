09f6747fcb804df84d44314325de690b7e9b9d39
Sangha Lee <sanghalee@Sanghas-MacBook-Pro.local>
2020-12-05 14:18:40 -0500

    implement loading profile functionality

--- Spot/Controller/AccountViewController.swift
+++ Spot/Controller/AccountViewController.swift
@@ -8,20 +8,48 @@
 import UIKit
 
 class AccountViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var benchPressMaxLabel: UILabel!
+    @IBOutlet weak var deadliftMaxLabel: UILabel!
+    @IBOutlet weak var squatMaxLabel: UILabel!
+    @IBOutlet weak var totalMaxLabel: UILabel!
     
     var user: User!
     
     override func viewWillAppear(_ animated: Bool) {
         // set up navigation controller title
         self.navigationController?.navigationBar.topItem?.title = "Account"
+        
+        user.loadData {
+            print("User loaded: \(self.user.uid)")
+            self.updateUI()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if user == nil {
-            user = User()
-        }
+        user = User()
     }
     
+    func updateUI() {
+        nameLabel.text = "\(user.firstName) \(user.lastName)"
+        emailLabel.text = user.email
+        dateOfBirthLabel.text = user.dateOfBirth
+        heightLabel.text = "\(user.height) cm"
+        weightLabel.text = "\(user.weight) kg"
+        bmiLabel.text = ""
+        benchPressMaxLabel.text = "\(user.benchPressMax) kg"
+        deadliftMaxLabel.text = "\(user.deadliftMax) kg"
+        squatMaxLabel.text = "\(user.squatMax) kg"
+        totalMaxLabel.text = " kg"
+    }
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+    }
 }
